3c608b34c6eced55b58a9c2187385544451d5db5
Pedro <chapesfroid2@gmail.com>
2021-11-23 12:34:07 -0300

    Ping Pong

--- main.py
+++ main.py
@@ -17,10 +17,14 @@ collision_other_rect = False
 collision_player = False
 collision_enemy = False
 speed = 5
-speed_ball = 3 * random.choice((1, -1))
+
+f12 = 0
+
+speed_ball = 5 * random.choice((1, -1))
 aleatorio = random.randint(0, 1)
 
-
+gameover = False
+pause = False
 
 Player1 = pygame.Rect(50, screen.get_height()/2 - player_height/2, 5, player_height)
 enemy = pygame.Rect(screen.get_width() - 50, screen.get_height()/2 - enemy_height/2, 5, enemy_height)
@@ -30,7 +34,8 @@ Ball = pygame.Rect(screen.get_width() / 2 - width_ball / 2, screen.get_height() 
 class Game():
     def __init__(self, *groups):
         super().__init__(*groups)
-        
+
+        self.player_ia_active = False
         self.ball_reset = False
         if aleatorio == 1:
             self.y_speed = -speed_ball
@@ -40,17 +45,19 @@ class Game():
             self.x_speed = +speed_ball
         self.player_point = 0
         self.enemy_point = 0
-    def moving_player(self):
-        key = pygame.key.get_pressed()
-        if key[pygame.K_d] or key[pygame.K_RIGHT]:
-            Player1.y -= speed
-        if key[pygame.K_a] or key[pygame.K_LEFT]:
-            Player1.y += speed
 
-        if Player1.y == 0:
-            Player1.y = 5
-        elif Player1.y >= screen.get_height() - 100:
-            Player1.y = screen.get_height() - 100
+    def moving_player(self):
+        if not self.player_ia_active:
+            key = pygame.key.get_pressed()
+            if key[pygame.K_d] or key[pygame.K_RIGHT]:
+                Player1.y -= speed
+            if key[pygame.K_a] or key[pygame.K_LEFT]:
+                Player1.y += speed
+
+            if Player1.y == 0:
+                Player1.y = 5
+            elif Player1.y >= screen.get_height() - 100:
+                Player1.y = screen.get_height() - 100
 
 
     def ball(self):
@@ -68,7 +75,25 @@ class Game():
             self.enemy_point += 1
             self.ball_reset = True
         
-            
+    def player_ia(self):
+        if self.player_ia_active:
+            if Ball.x <= screen.get_width() / 2:
+                if Player1.y < Ball.y:
+                    Player1.y += speed
+                elif Player1.y > Ball.y:
+                    Player1.y -= speed
+                else:
+                    Player1.y += 0
+                
+                if Player1.y <= 0:
+                    Player1.y = 10
+                elif Player1.y >= screen.get_height()-100:
+                    Player1.y = screen.get_height()-100
+            else:
+                if Player1.y > int(screen.get_height() / 2 - player_height/2)-5:
+                    Player1.y -= speed
+                elif Player1.y < int(screen.get_height()/2 - player_height/2) - 5:
+                    Player1.y += speed
     def enemy_ia(self):
         if Ball.x >= screen.get_width() / 2:
             if enemy.y < Ball.y:
@@ -89,7 +114,10 @@ class Game():
                 enemy.y += speed
 
     def update(self):
-        self.moving_player()
+        if not self.player_ia_active:
+            self.moving_player()
+        else:
+            self.player_ia()
         self.enemy_ia()
         self.ball()
 
@@ -114,13 +142,14 @@ while True:
     draw_text(str(game.enemy_point), font, (255, 255, 255), screen, screen.get_width()/2 + 100, screen.get_height()/2- 230)
 
     # linha no meio
-    pygame.draw.rect(screen, (255, 255, 255), (screen.get_width()/2, 0, 2, screen.get_height()))
 
     pygame.draw.rect(screen, (255, 255, 255), Ball)
     pygame.draw.rect(screen, (255, 255, 255), enemy)
 
     # suposto player 1
     pygame.draw.rect(screen, (255, 255, 255), Player1)
+
+    pygame.draw.rect(screen, (255, 255, 255), (screen.get_width()/2, 0, 2, screen.get_height()))
 
     if Ball.colliderect(Player1) or Ball.colliderect(enemy):
         game.x_speed *= -1
@@ -141,8 +170,18 @@ while True:
             if event.key == K_ESCAPE:
                 pygame.quit()
                 sys.exit()
+            if event.key == K_F12:
+                f12 += 1
+                if f12 == 1:
+                    game.player_ia_active = True
+                elif f12 == 2:
+                    game.player_ia_active = False
+                    f12 = 0
 
-    
-    game.update()
+
     clock.tick(60)
-    pygame.display.flip()
+
+    if not gameover:
+        if not pause:
+            game.update()
+            pygame.display.flip()
