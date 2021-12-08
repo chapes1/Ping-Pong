43d16ad948706066c95b9777f6830c5c0032f156
chapes1 <chapesfroid2@gmail.com>
2021-12-07 22:37:05 -0300

    Ping Pong

--- main.py
+++ main.py
@@ -1,142 +1,73 @@
 import pygame
-from pygame.locals import *
 import sys
+from pygame.locals import *
 import random
 from teste import *
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 500))
-font = pygame.font.SysFont('Bebas.ttf', 32)
-clock = pygame.time.Clock()
-gameover_text = "GAME OVER"
+resolution = pygame.display.Info()
+width = resolution.current_w
+height = resolution.current_h
 
-Player1 = pygame.Rect(50, screen.get_height()/2 - player_height/2, 5, player_height)
-enemy = pygame.Rect(screen.get_width() - 50, screen.get_height()/2 - enemy_height/2, 5, enemy_height)
-Ball = pygame.Rect(screen.get_width () / 2 - width_ball / 2, screen.get_height() / 2 - height_ball/2, width_ball, height_ball)
+screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
 
-# Botao para baixo
-image_move_down = pygame.image.load('button_down.png').convert_alpha()
-image_move_down = pygame.transform.scale(image_move_down, (50, 50))  
+x = 10 * width
+enemy_height = x / 100
+player_width = x / 100
 
-# Botao para cima
-image_move_up = pygame.image.load('button_up2.png').convert_alpha()
-image_move_up = pygame.transform.scale(image_move_up, (50, 50))
+metade_com_player_height = screen.get_height() - 100
+metade_com_player_width = screen.get_width()/2 - player_width / 2
+
+Enemy = pygame.Rect(metade_com_player_width, 100, player_width, 2)
+Player = pygame.Rect(metade_com_player_width, metade_com_player_height, player_width, 2)
+
+Ball = pygame.Rect(screen.get_width()/2 - width_ball/2, screen.get_height()/2 - height_ball/2, width_ball, height_ball)
+
+while True:
+    screen.fill((52, 63, 82)) 
+    pygame.display.set_caption("Ping Pong")
+    mx, my = pygame.mouse.get_pos()
+
+    #Desenhando os pontos de cada jogador
+    draw_text('P: '+str(game.player_point), font_number, (255,255,255), screen, 100, 50)
+    draw_text('E: '+str(game.enemy_point), font_number, (255,255,255), screen, screen.get_width()-100,50)
 
 
-click_move_down = pygame.Rect(100, screen.get_height()-60, 50, 50)
-click_move_up = pygame.Rect(screen.get_width()-100, screen.get_height()/2+190, 50, 50)
+    # Personagens
+    pygame.draw.rect(screen, (255, 255, 255), (5, height/2, width, 2))
+    pygame.draw.rect(screen, (255, 255, 255), Player)
+    pygame.draw.rect(screen, (255, 255, 255), Enemy)
+    pygame.draw.rect(screen, (255, 255, 255), Ball, border_radius=50)
 
+    if Ball.colliderect(Player) or Ball.colliderect(Enemy):
+        game.y_speed *= -1
+        
+    if game.ball_reset:
+        Ball.x = screen.get_width() / 2 - width_ball / 2
+        Ball.y = screen.get_height() / 2 - height_ball / 2
 
-def GameOver():
-    while game.gameover:
-        image = pygame.image.load('back.png').convert_alpha()
-        image = pygame.transform.scale(image, (screen.get_width(), screen.get_height()))
-        screen.blit(image, (0,0))
-        draw_text(gameover_text, font, (0,255,0), screen,screen.get_width()/2-70, screen.get_height()/2-50)
-        draw_text('para jogar novamente aperte ESPAÇO, para SAIR aperte ESC', 
-                font, (0,255,0), screen,screen.get_width()/2-320, screen.get_height()/2)    
-
-        pygame.display.update()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        game.x_speed *= random.choice((1, -1))
+        game.y_speed *= random.choice((1, -1))
+        game.ball_reset = False
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
-                if event.key == K_SPACE:
-                    pygame.display.flip()
-                    game.gameover = False
-                    game.enemy_point = 0
-                    game.player_point = 0
-                    Player1.x = 50
-                    Player1.y = screen.get_height()/2 - player_height/2
-                    main()
-                    #para jogar novamente aperte ESPAÇO, para SAIR aperte ESC
+            if event.key == K_F12:
+                f12 += 1
+                if f12 == 1:
+                    game.player_ia_active = True
+                elif f12 == 2:
+                    game.player_ia_active = False
+                    f12 = 0
 
-game = Game()
-def main():
-    f12 = 0
-    click = False
-    while True:    
-        GameOver()
-        fps = int(clock.get_fps())
-        pygame.display.set_caption("Ping Pong - " + "FPS: " + str(fps))
-
-        mx, my = pygame.mouse.get_pos()
-
-        screen.fill((52, 63, 82))
-
-        if game.enemy_point >= 5:
-            game.gameover = True
-
-        draw_text(str(game.player_point), font, (255, 255, 255), screen, screen.get_width()/2 - 100, screen.get_height()/2- 230)
-        draw_text(str(game.enemy_point), font, (255, 255, 255), screen, screen.get_width()/2 + 100, screen.get_height()/2- 230)
-
-
-        # linha no meio
-        pygame.draw.rect(screen, (255, 255, 255), Ball)
-        pygame.draw.rect(screen, (255, 255, 255), enemy)
-
-        # suposto player 1
-        pygame.draw.rect(screen, (255, 255, 255), Player1)
-
-        pygame.draw.rect(screen, (255, 255, 255), (screen.get_width()/2, 0, 2, screen.get_height()))
-
-        # botão para mover para baixos
-        pygame.draw.rect(screen, (52, 63, 82), click_move_down)
-        screen.blit(image_move_down, (100, screen.get_height()-60))
-
-        #botao para mover para baixo
-        pygame.draw.rect(screen, (52, 63, 82), click_move_up)
-        screen.blit(image_move_up, (screen.get_width()-100, screen.get_height()/2+190))
-
-
-        if click_move_down.collidepoint((mx, my)):          
-            if click:    
-                Player1.y += 5
-        if click_move_up.collidepoint((mx, my)):          
-            if click:    
-                Player1.y -= 5
-
-        if Ball.colliderect(Player1) or Ball.colliderect(enemy):
-            game.x_speed *= -1
-
-        if game.ball_reset:
-            Ball.x = screen.get_width() / 2 - width_ball / 2
-            Ball.y = screen.get_height() / 2 - height_ball / 2
-
-            game.x_speed *= random.choice((1, -1))
-            game.y_speed *= random.choice((1, -1))
-            game.ball_reset = False
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == KEYDOWN:   
-                if event.key == K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
-                if event.key == K_F12:
-                    f12 += 1
-                    if f12 == 1:
-                        game.player_ia_active = True
-                    elif f12 == 2:
-                        game.player_ia_active = False
-                        f12 = 0
-            if event.type == MOUSEBUTTONDOWN:
-                click = True
-            if event.type == MOUSEBUTTONUP:
-                click = False
-                
-        clock.tick(90)
-
-        if not game.gameover:
-            game.update(Ball=Ball, screen=screen, enemy=enemy,Player1=Player1)
-        pygame.display.flip()
-
-if __name__ == '__main__':
-    main()
+    if not End:
+        game.update(Ball=Ball, screen=screen, enemy=Enemy,Player1=Player, player_width=player_width, enemy_height=enemy_height)
+    clock.tick(60)
+    pygame.display.update()
--- teste.py
+++ teste.py
@@ -1,23 +1,33 @@
 import pygame
 from pygame.locals import *
-import sys
 import random
 
+clock = pygame.time.Clock()
+
+speed = 7
+
+aleatorio = random.randint(0, 1)
+
+width_ball = 15
+height_ball = 15
 
 pygame.init()
 
-speed = 5
+End = False
 
-aleatorio = random.randint(0, 1)
-enemy_height = 100
-player_height = 100
-width_ball = 15
-height_ball = 15
+f12 = 0
+collide = 0
+collide_enemy = 0
+# FONTS
+font = pygame.font.SysFont('fonts/Bebas.ttf', 32)
+font_number = pygame.font.SysFont('fonts/One.ttf', 50)
+font_number2 = pygame.font.SysFont('fonts/TitilliumWeb-Regular.ttf', 50)
+
 
 class Game():
     def __init__(self, *groups):
         super().__init__(*groups)
-        self.speed_ball = 4 * random.choice((1, -1))
+        self.speed_ball = 5 * random.choice((1, -1))
         self.gameover = False
         self.player_ia_active = False
         self.ball_reset = False
@@ -30,86 +40,76 @@ class Game():
         self.player_point = 0
         self.enemy_point = 0
 
-    def moving_player(self, Player1, screen):
+    def moving_player(self, Player1, screen, player_width):
         if not self.player_ia_active:
             key = pygame.key.get_pressed()
             if key[pygame.K_d] or key[pygame.K_RIGHT]:
-                Player1.y -= speed
+                Player1.x += speed
             if key[pygame.K_a] or key[pygame.K_LEFT]:
-                Player1.y += speed
+                Player1.x -= speed
 
-            if Player1.y == 0:
-                Player1.y = 5
-            elif Player1.y >= screen.get_height() - 100:
-                Player1.y = screen.get_height() - 100
+            if Player1.x <= 0:
+                Player1.x = 5
+            elif Player1.x >= screen.get_width() - player_width:
+                Player1.x = screen.get_width() - player_width
 
 
     def ball(self, Ball, screen):
         Ball.x += self.x_speed
         Ball.y -= self.y_speed
 
-        if Ball.y >= screen.get_height() - 15 or Ball.y <= 0:
-            self.y_speed *= -1
+        if Ball.x >= screen.get_width() - width_ball or Ball.x <= 0:
+            self.x_speed *= -1
 
-        if Ball.x <= 0:
+        if Ball.y <= 0:
             self.enemy_point += 1
             self.ball_reset = True
 
-        elif Ball.x >= screen.get_width():
+        elif Ball.y >= screen.get_height():
             self.player_point += 1
             self.ball_reset = True
         
-    def player_ia(self, Ball, screen, Player1):
+    def player_ia(self, Ball, screen, Player1, player_width):
         if self.player_ia_active:
-            if Ball.x <= screen.get_width() / 2:
-                if Player1.y < Ball.y:
-                    Player1.y += speed
-                elif Player1.y > Ball.y:
-                    Player1.y -= speed
+            if Ball.y >= screen.get_height() / 2:
+                if Player1.x < Ball.x:
+                    Player1.x += speed
+                elif Player1.x > Ball.x:
+                    Player1.x -= speed
                 else:
-                    Player1.y += 0
+                    Player1.x += 0
                 
-                if Player1.y <= 0:
-                    Player1.y = 10
-                elif Player1.y >= screen.get_height()-100:
-                    Player1.y = screen.get_height()-100
+                if Player1.x <= 0:
+                    Player1.x = 10
+                elif Player1.x >= screen.get_width()-player_width:
+                    Player1.x = screen.get_width()-player_width
 
             else:
 
-                if Player1.y > int(screen.get_height() / 2 - player_height/2)-5:
-                    Player1.y -= speed
-                elif Player1.y < int(screen.get_height()/2 - player_height/2) - 5:
-                    Player1.y += speed
+                if Player1.x > int(screen.get_width() / 2 - player_width/2)-5:
+                    Player1.x -= speed
+                elif Player1.x < int(screen.get_width()/2 - player_width/2) - 5:
+                    Player1.x += speed
 
-    def enemy_ia(self, Ball, screen, enemy):
-        if Ball.x >= screen.get_width() / 2:
-            if enemy.y < Ball.y:
-                enemy.y += speed
-            elif enemy.y > Ball.y:
-                enemy.y -= speed
-            else:
-                enemy.y += 0
+    def enemy_ia(self, Ball, screen, enemy, enemy_height):
+        if enemy.x < Ball.x:
+            enemy.x += speed
+        elif enemy.x > Ball.x:
+            enemy.x -= speed
+        else:
+            enemy.x += 0
             
-            if enemy.y <= 0:
-                enemy.y = 10
-            elif enemy.y >= screen.get_height()-100:
-                enemy.y = screen.get_height()-100
-        else:
-            if enemy.y > int(screen.get_height() / 2 - enemy_height/2)-5:
-                enemy.y -= speed
-            elif enemy.y < int(screen.get_height()/2 - enemy_height/2) - 5:
-                enemy.y += speed
+        if enemy.x <= 0:
+            enemy.x = enemy_height
+        elif enemy.x >= screen.get_width()-enemy_height:
+            enemy.x = screen.get_width()-enemy_height
     
-        
-    def button_down(self):
-        pass
-
-    def update(self, Ball, enemy, Player1, screen):
+    def update(self, Ball, enemy, Player1, screen, enemy_height, player_width):
         if not self.player_ia_active:
-            self.moving_player(Player1=Player1, screen=screen)
+            self.moving_player(Player1=Player1, screen=screen, player_width=player_width)
         else:
-            self.player_ia(Ball=Ball, screen=screen,Player1=Player1)
-        self.enemy_ia(Ball=Ball, screen=screen, enemy=enemy)
+            self.player_ia(Ball=Ball, screen=screen,Player1=Player1, player_width=player_width)
+        self.enemy_ia(Ball=Ball, screen=screen, enemy=enemy, enemy_height=enemy_height)
         self.ball(Ball=Ball, screen=screen)
 
 def draw_text(text, fonte, color, surface, x, y):
@@ -118,3 +118,4 @@ def draw_text(text, fonte, color, surface, x, y):
     textrect.topleft = (x, y)
     surface.blit(textobj, textrect)
     
+game = Game()
